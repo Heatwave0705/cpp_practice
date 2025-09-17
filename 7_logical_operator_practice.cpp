5fed7216db99ad2e0b6c171a4867d992e68b240b
Heatwave0705 <17793dxbp@gmail.com>
2025-09-18 00:00:52 +0400

    practicing logical operators

---
+++ 7_logical_operator_practice.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    bool cloudy;
+    bool rainy;
+    cloudy = true;
+    rainy = true;
+    if (rainy && cloudy) {
+        std::cout<< "it is stormy";
+    }
+}
